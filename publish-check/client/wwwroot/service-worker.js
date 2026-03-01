1599578dfddf1d870eb542d6c5547df74282d719
Cuong Nguyen <99233198+chicuong2k3@users.noreply.github.com>
2026-03-02 05:18:06 +0700

    Delete publish-check/client directory

--- publish-check/client/wwwroot/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.assetsInclude = [/\.dll$/, /\.wasm$/, /\.js$/, /\.json$/, /\.css$/, /\.html$/, /\.dat$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.svg$/, /\.ico$/, /\.woff2?$/, /\.ttf$/, /\.webp$/];
-self.assetsExclude = [/^_content\/Bit\.Bswup\/bit-bswup\.sw\.js$/, /^service-worker\.js$/];
-self.defaultUrl = '/';
-self.serverHandledUrls = [/\/api\//, /\/graphql$/];
-self.assetsUrl = '/service-worker-assets.js';
-self.caseInsensitiveUrl = true;
-self.enableIntegrityCheck = true;
-
-self.importScripts('_content/Bit.Bswup/bit-bswup.sw.js');
